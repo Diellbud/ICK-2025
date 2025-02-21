536462086e0eebcbf6cb9be6dde215f3a8512ded
Diell <budimadiell@gmail.com>
2025-02-21 19:16:41 +0100

    11-IntroToDOM

---
+++ JavaScript/DOM/11-IntroToDOM/script.js
@@ -0,0 +1,6 @@
+console.log(window);
+console.log(window.document);
+console.dir(window.document);
+console.dir(window.document.body);
+document.body.innerHTML = "<h1>Replaced</h1>";
+alert("working");
