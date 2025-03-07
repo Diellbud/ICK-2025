8499b91140bab4e56de46d40db474377b1fdaefa
Diell <budimadiell@gmail.com>
2025-03-07 20:05:01 +0100

    13-LocalStorage

--- JavaScript/12-Events/CreateUpdateReadDelete/script.js
+++ JavaScript/12-Events/CreateUpdateReadDelete/script.js
@@ -1,26 +1,20 @@
+const saveStudents = JSON.parse(localStorage.getItem("students"));
+const initialStudents = saveStudents || []
 const createForm = document.getElementById("createNewStudent");
-let firstNameError = document.getElementById('firstNameErrorId')
-let lastNameError = document.getElementById('lastNameErrorId')
-const submitButton = document.getElementById('submitButton')
+let firstNameError = document.getElementById("firstNameErrorId");
+let lastNameError = document.getElementById("lastNameErrorId");
 
+// submitButton.disabled = true;
+firstNameError.textContent = "";
+lastNameError.textContent = "";
 
-submitButton = true
-firstNameError.textContent = ''
-lastNameError.textContent = ''
+let students = initialStudents
 
-let students = [
-  {
-    id: 1,
-    firstName: "John",
-    lastName: "Doe",
-  },
-  {
-    id: 2,
-    firstName: "James",
-    lastName: "Smith",
-  },
-];
-let newId= students.length + 1
+const saveStudentsLocalStorage = () =>{
+  localStorage.setItem('students', JSON.stringify(students))
+}
+
+let newId = students.length + 1;
 const readList = () => {
   const tableBody = document.getElementById("studentsList");
   tableBody.innerHTML = ``;
@@ -39,47 +33,46 @@ const readList = () => {
   });
 };
 
-readList();
 const onCreateNewStudent = (e) => {
   e.preventDefault();
-  const firstName = document.getElementById('firstName')
-  const lastName = document.getElementById('lastName')
-
-const studentObject = {
-  id: newId++, 
-  firstName:lastName.value,
-  lastName:lastName.value
-}
-students.push(studentObject)
-if (firstName.value == "") {
-  firstNameError.textContent = "This feild is required"
-}else{
-    submitButton.disabled = false
-    firstNameError.textContent = ''
-}
-if (lastName.value == "") {
-  lastNameError.textContent = "This feild is required"
-}else{
-  submitButton.disabled = false
-    lastNameError.textContent = ''
-}
-firstName.value = ""
-lastName.value = ""
-
-readList()
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const studentObject = {
+    id: newId++,
+    firstName: lastName.value,
+    lastName: lastName.value,
+  };
+  students.push(studentObject);
+  saveStudentsLocalStorage()
+  if (firstName.value == "") {
+    firstNameError.textContent = "This feild is required";
+  } else {
+    firstNameError.textContent = "";
+  }
+  if (lastName.value == "") {
+    lastNameError.textContent = "This feild is required";
+  } else {
+    lastNameError.textContent = "";
+  }
+  firstName.value = "";
+  lastName.value = "";
+  readList();
 };
 
-const deleteStudent = (id) =>{
-    const index = students.findIndex(student => student.id === id)
-    const deleteStudent = students.splice(index, 1)[0]
-    return deleteStudent
-}
+const deleteStudent = (id) => {
+  const index = students.findIndex((student) => student.id === id);
+  const deleteStudent = students.splice(index, 1)[0];
+  saveStudentsLocalStorage()
+  return deleteStudent;
+};
 
-const deleteStudentAction = (id) =>{
-    if (confirm(`are you sure you want to delete this student with id: ${id}?`)){
-        deleteStudent(id)
-        readList()
-    }
-}
+const deleteStudentAction = (id) => {
+  if (confirm(`are you sure you want to delete this student with id: ${id}?`)) {
+    deleteStudent(id);
+    readList();
+  }
+};
 
+readList();
 createForm.addEventListener("submit", onCreateNewStudent);
+saveStudentsLocalStorage()
