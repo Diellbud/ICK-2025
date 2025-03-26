57370564980d9944982bcac9738128cd14476f4b
Diell <budimadiell@gmail.com>
2025-03-26 19:33:08 +0100

    16-EcommerceExample usingWebpack

---
+++ JavaScript/18-basic-setup/src/about.js
@@ -0,0 +1,7 @@
+export default function about() {
+    const aboutContainer = document.createElement('div');
+    aboutContainer.innerHTML = `
+    <h1>welcome to about</h1>
+    `
+    return aboutContainer
+}
---
+++ JavaScript/18-basic-setup/src/home.js
@@ -0,0 +1,7 @@
+export default function home() {
+    const homeContainer = document.createElement('div');
+    homeContainer.innerHTML = `
+    <h1>welcome to home</h1>
+    `
+    return homeContainer
+}
